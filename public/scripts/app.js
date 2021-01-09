eb4d51964500b8c412dedbc170daef2a86c0c2fa
Nisarg <nisargvv@gmail.com>
2021-01-09 00:28:36 +0000

    Update height of #map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,10 +11,10 @@ $(() => {
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.036 };
+  const uluru = { lat: 43.08819077186206, lng: -79.07223055850586 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 6,
     center: uluru,
   });
   // The marker, positioned at Uluru
