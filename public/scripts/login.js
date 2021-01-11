68e2003db8a9d43e71025337debc35e280c6c304
justinkwanchan <justinkwanchan@gmail.com>
2021-01-11 20:24:59 +0000

    Replace css modification with add/removal of class for visibility - begin adding jQuery tree traversal

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
-  // $('#test').on('click', function(event) {
-  //   event.preventDefault();
-  //   $(this).parent().parent().parent().addClass("form-animate-left");
-  // });
-
   $('#test').on('click', function(event) {
     // event.preventDefault();
-    $('#sign-up-screen').css('visibility', 'visible');
-    $('#form').addClass("form-animate-left");
-    $('#form').removeClass("form-animate-right");
+    console.log($(this).parent().parent());
+    console.log($('#login-screen'));
+    // console.log($(this).parent().parent().parent());
+    // console.log($('#form'));
+    // $('#sign-up-screen').css('visibility', 'visible');
+    $('#sign-up-screen').removeClass('form-hidden');
+    $(this).parent().parent().parent().addClass("form-animate-left");
+    $(this).parent().parent().parent().removeClass("form-animate-right");
     setTimeout(function() {
-      $('#login-screen').css('visibility', 'hidden');
+      $('#login-screen').addClass('form-hidden');
     }, 400);
   });
 
   $('#test2').on('click', function(event) {
     // event.preventDefault();
-    $('#login-screen').css('visibility', 'visible');
-    $('#form').addClass("form-animate-right");
-    $('#form').removeClass("form-animate-left");
+    $('#login-screen').removeClass('form-hidden');
+    $(this).parent().parent().parent().addClass("form-animate-right");
+    $(this).parent().parent().parent().removeClass("form-animate-left");
     setTimeout(function() {
-      $('#sign-up-screen').css('visibility', 'hidden');
+      $('#sign-up-screen').addClass('form-hidden');
     }, 400);
   });
 });
