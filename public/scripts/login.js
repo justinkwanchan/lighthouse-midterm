575ce192a0611bc538aa4bf83b0ab93ff6507d22
justinkwanchan <justinkwanchan@gmail.com>
2021-01-12 00:45:48 +0000

    Add functionality to display and hide login overlay

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+  // Switches the login form to sign up form
   $('#switch-to-sign-up').on('click', function(event) {
     $('#sign-up-screen').removeClass('form-hidden');
     $(this).parent().parent().parent().addClass("form-animate-left");
@@ -8,6 +10,7 @@ $(document).ready(function() {
     }, 400);
   });
 
+  // Switches the sign up form to login form
   $('#switch-to-login').on('click', function(event) {
     $('#login-screen').removeClass('form-hidden');
     $(this).parent().parent().parent().addClass("form-animate-right");
@@ -15,5 +18,22 @@ $(document).ready(function() {
     setTimeout(function() {
       $('#sign-up-screen').addClass('form-hidden');
     }, 400);
+  });
+
+  // Displays the login form overlay - closes it upon mouse click outside the form
+  $('#login-button').on('click', function(event) {
+    event.stopPropagation();
+
+    $('#login-overlay').removeClass("overlay-remove");
+    $('#login-overlay').addClass("overlay-display");
+
+    $(document.body).on('click', function(event) {
+      $('#login-overlay').addClass("overlay-remove");
+      $('#login-overlay').removeClass("overlay-display");
+    });
+
+    $("#login-overlay").on('click', function(event) {
+      event.stopPropagation();
+    });
   });
 });
