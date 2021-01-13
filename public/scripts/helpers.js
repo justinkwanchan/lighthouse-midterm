80223dc154f954cc194bb58a4fafeac260c65218
justinkwanchan <justinkwanchan@gmail.com>
2021-01-13 01:34:48 +0000

    Refactor db query into getUserWithEmail helper function

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,3 +1,13 @@
+// Generate a random 6-character-long alphanumeric string
+const generateRandomString = function () {
+  const charStr = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let alphaNum = '';
+  for (let i = 0; i < 6; i++) {
+    alphaNum += charStr.charAt(Math.floor(Math.random() * charStr.length));
+  }
+  return alphaNum;
+};
+
 // Add user to database
 const addUser = function (db, user) {
   return db.query(`
@@ -9,13 +19,18 @@ const addUser = function (db, user) {
     .catch(err => console.error('query error', err.stack))
 };
 
-const getUserWithEmail = function (data, email) {
-  for (const row of data) {
-    if (row.email === email) {
-      return row;
-    }
-  }
-  return null;
+//
+const getUserWithEmail = function (db, email) {
+  return db.query(`SELECT * FROM users;`)
+    .then(data => {
+      for (const row of data.rows) {
+        if (row.email === email) {
+          return row;
+        }
+      }
+      return null;
+    })
+    .catch(err => console.error('query error:', err.stack));
 };
 
 module.exports = { addUser, getUserWithEmail };
--- routes/usersdb.js
+++ routes/usersdb.js
@@ -6,13 +6,13 @@ module.exports = (db) => {
   const { addUser, getUserWithEmail } = require('../public/scripts/helpers');
 
   router.post("/", (req, res) => {
-    db.query(`SELECT * FROM users;`).then(data => {
+
       // Create function for registration and another for login
 
       // Registration
       if (!req.body.email || !req.body.password) {
         res.status(400).send('<h2>400 - Email or password left empty</h2>');
-      } else if (getUserWithEmail(data.rows, req.body.email) !== null) {
+      } else if (getUserWithEmail(db, req.body.email) !== null) {
         res.status(400).send('<h2>400 - Email is taken</h2>');
       } else {
         addUser(db, req.body);
@@ -21,8 +21,7 @@ module.exports = (db) => {
         console.log(req.params);
         res.redirect("/");
       }
-    })
-    .catch(err => console.error('query error:', err.stack));
+
   });
 
   return router;
