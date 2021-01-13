65ada2900e5964dceeb74fa5ec9f9f605624cdfe
justinkwanchan <justinkwanchan@gmail.com>
2021-01-13 05:08:05 +0000

    Implement login function

--- routes/usersdb.js
+++ routes/usersdb.js
@@ -8,19 +8,36 @@ module.exports = (db) => {
   // Register a new email
   router.post("/register", (req, res) => {
     db.query(`SELECT * FROM users;`).then(data => {
-      // Create function for registration and another for login
-
-      // Registration
       if (!req.body.email || !req.body.password) {
         res.status(400).send('<h2>400 - Email or password left empty</h2>');
       } else if (getUserWithEmail(data.rows, req.body.email) !== null) {
-        res.status(400).send('<h2>400 - Email is taken</h2>');
+        res.status(400).send('<h2>403 - Email is taken</h2>');
       } else {
         const newSessionID = generateRandomString();
         const userData = { newSessionID, ...req.body };
 
         addUser(db, userData);
         req.session.user_id = newSessionID;
+
+        res.redirect("/");
+      }
+    })
+    .catch(err => console.error('query error:', err.stack));
+  });
+
+  // Login
+  router.post("/login", (req, res) => {
+    db.query(`SELECT * FROM users;`).then(data => {
+      const user = getUserWithEmail(data.rows, req.body.email);
+
+      if (!req.body.email || !req.body.password) {
+        res.status(400).send('<h2>400 - Email or password left empty</h2>');
+      } else if (user === null) {
+        res.status(403).send('<h2>403 - Email does not exist</h2>');
+      } else if (req.body.password !== user.password) {
+        res.status(403).send('<h2>403 - Password does not match</h2>');
+      } else {
+        req.session.user_id = user.session_id;
 
         res.redirect("/");
       }
