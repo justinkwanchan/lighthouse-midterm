30d9a703d3a250cf567b92c4d0cb1e6eaf56097d
justinkwanchan <justinkwanchan@gmail.com>
2021-01-13 02:10:43 +0000

    Unrefactor db query from helper function

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -11,8 +11,8 @@ const generateRandomString = function () {
 // Add user to database
 const addUser = function (db, user) {
   return db.query(`
-    INSERT INTO users (email, password)
-    VALUES ($1, $2)
+    INSERT INTO users (session_id, email, password)
+    VALUES ($1, $2, $3)
     RETURNING *;
   `, Object.values(user))
     .then(res => res.rows[0])
@@ -20,21 +20,13 @@ const addUser = function (db, user) {
 };
 
 //
-const getUserWithEmail = function (db, email) {
-  return db.query(`SELECT * FROM users;`)
-    .then(data => {
-      for (const row of data.rows) {
-        console.log(row.email);
-        console.log(email);
-        console.log(row.email === email);
-        if (row.email === email) {
-          return row;
-        }
-      }
-      console.log(null);
-      return null;
-    })
-    .catch(err => console.error('query error:', err.stack));
+const getUserWithEmail = function (data, email) {
+  for (const row of data) {
+    if (row.email === email) {
+      return row;
+    }
+  }
+  return null;
 };
 
 module.exports = { generateRandomString, addUser, getUserWithEmail };
--- routes/usersdb.js
+++ routes/usersdb.js
@@ -6,26 +6,26 @@ module.exports = (db) => {
   const { generateRandomString, addUser, getUserWithEmail } = require('../public/scripts/helpers');
 
   router.post("/", (req, res) => {
-
+    db.query(`SELECT * FROM users;`).then(data => {
       // Create function for registration and another for login
 
       // Registration
       if (!req.body.email || !req.body.password) {
         res.status(400).send('<h2>400 - Email or password left empty</h2>');
-      } else if (getUserWithEmail(db, req.body.email) !== null) {
-        console.log('fail null');
+      } else if (getUserWithEmail(data.rows, req.body.email) !== null) {
         res.status(400).send('<h2>400 - Email is taken</h2>');
       } else {
+        addUser(db, req.body);
         const newSessionID = generateRandomString();
-        const userData = { newSessionID, ...req.body }
+        const userData = { newSessionID, ...req.body };
         addUser(db, userData);
         req.session.user_id = newSessionID;
         console.log(req.session);
-        console.log(userData);
         // console.log(req.params);
         res.redirect("/");
       }
-
+    })
+    .catch(err => console.error('query error:', err.stack));
   });
 
   return router;
