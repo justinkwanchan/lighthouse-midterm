7f9fec7e06a9438621bcffb404f052aaf6a301e0
justinkwanchan <justinkwanchan@gmail.com>
2021-01-13 21:15:22 +0000

    Remove name from users db table

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -3,10 +3,6 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   session_id VARCHAR(255) PRIMARY KEY NOT NULL,
-<<<<<<< HEAD
-=======
-  name VARCHAR(255) DEFAULT 'Test Name',
->>>>>>> 5b584a71c358b6c50b30bf16ddec0966b94fbe0d
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
