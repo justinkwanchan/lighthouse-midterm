b7ff228d590cc91146e16569fd2aa9d7cb935ac1
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-13 14:14:48 -0500

    Use addPins to store data in midterm database

--- routes/fav_users.js
+++ routes/fav_users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 let dataBase = { title: "narnia", lat: "0.0", lng: "0.0" };
 module.exports = (db) => {
+
+  const { addPins } = require('../public/scripts/helpers');
+
   router.post("/", (req, res) => {
     let location = req.body['pins-name'];
     let lat = req.body['pins-lat'];
@@ -9,6 +12,7 @@ module.exports = (db) => {
     dataBase = { title: location, lat: lat, lng: lng };
     console.log("*************************************");
     console.log(dataBase);
+    addPins(db, dataBase);
     res.redirect("/");
   });
   return router;
