3dc89acff75f540b5f4ba6cc8ea9e03bfece0664
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-13 15:57:18 -0500

    Make session_id PK in users db table

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -3,6 +3,6 @@
 DROP TABLE IF EXISTS widgets CASCADE;
 CREATE TABLE widgets (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
+  user_id VARCHAR(255) REFERENCES users(session_id),
   name VARCHAR(255) NOT NULL
 );
