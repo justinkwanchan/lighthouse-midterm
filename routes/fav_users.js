56450a1fad8ad7526b494037c22abf84210b8862
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-13 16:04:51 -0500

    Add list name to the form

--- routes/fav_users.js
+++ routes/fav_users.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
-let dataBase = { title: "narnia", lat: "0.0", lng: "0.0" };
+let dataBase = { userId: "34r5dw3", list: "myList", title: "narnia", lat: "0.0", lng: "0.0" };
 module.exports = (db) => {
 
   const { addPins } = require('../public/scripts/helpers');
 
   router.post("/", (req, res) => {
+    let userId = req.session.user_id;
+    let list = req.body['pins-list'];
     let location = req.body['pins-name'];
     let lat = req.body['pins-lat'];
     let lng = req.body['pins-lng'];
-    dataBase = { title: location, lat: lat, lng: lng };
+    dataBase = { userId: userId, list: list, title: location, lat: lat, lng: lng };
     console.log("*************************************");
     console.log(dataBase);
     addPins(db, dataBase);
