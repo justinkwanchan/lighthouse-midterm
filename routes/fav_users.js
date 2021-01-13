0eb6e4251692969b3bdc453eccf598c97d51cd93
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-12 19:24:07 -0500

    Update dataBase

--- routes/fav_users.js
+++ routes/fav_users.js
@@ -3,13 +3,13 @@ const router = express.Router();
 let dataBase = { title: "narnia", lat: "0.0", lng: "0.0" };
 module.exports = (db) => {
   router.post("/", (req, res) => {
-    let name = req.body.name;
-    let lat = req.body.lat;
-    let lng = req.body.lng;
-    dataBase = { title: name, lat: lat, lng: lng };
+    let location = req.body['pins-name'];
+    let lat = req.body['pins-lat'];
+    let lng = req.body['pins-lng'];
+    dataBase = { title: location, lat: lat, lng: lng };
     console.log("*************************************");
-    console.log(req.body);
-    res.send("/");
+    console.log(dataBase);
+    res.redirect("/");
   });
   return router;
 };
