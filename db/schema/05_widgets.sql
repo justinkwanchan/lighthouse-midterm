e57e47895b401d623330ed8bf627d602a453b647
justinkwanchan <justinkwanchan@gmail.com>
2021-01-14 21:36:04 +0000

    Add maps db table and update table columns

--- db/schema/05_widgets.sql
+++
@@ -1,8 +0,0 @@
--- Drop and recreate Widgets table (Example)
-
-DROP TABLE IF EXISTS widgets CASCADE;
-CREATE TABLE widgets (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id VARCHAR(255) REFERENCES users(session_id),
-  name VARCHAR(255) NOT NULL
-);
