23be21822c449325f5f8559902e296b65ba64a72
Nisarg Vadgama <nisargvv@gmail.com>
2021-01-14 22:34:30 -0500

    Relace narnia with horseshoe falls

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO maps (user_id, name) VALUES ('h3o28d', 'Narnia');
+INSERT INTO maps (user_id, name) VALUES ('h3o28d', 'Horseshoe Falls');
 INSERT INTO maps (user_id, name) VALUES ('h3o28d', 'Atlantis');
--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO pins (map_id, name, description, icon, latitude, longitude) VALUES ('1', 'Narnia', 'Fantasy', 'sports', '0.0', '0.0');
+INSERT INTO pins (map_id, name, description, icon, latitude, longitude) VALUES ('1', 'Horseshoe Falls', 'Fantasy', 'sports', '43.088456984465104', '-79.07431008007899');
 INSERT INTO pins (map_id, name, description, icon, latitude, longitude) VALUES ('1', 'Ontario', 'Description test', 'cafe', '43.15239568975307', '-78.95607303375252');
